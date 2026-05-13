a2d563e0dd237bf2d161b57aef32525d023d6cfb
CodeCaptain.crx <it@crionyx.com>
2026-05-13 10:45:01 -0500

    eliminados

--- backend/src/routes/documentos.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const controller = require("../controllers/documentos.controller");
-
-router.get("/documentos", controller.getDocumentos);
-
-module.exports = router;
